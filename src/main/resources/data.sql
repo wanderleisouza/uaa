d1ca1dec851f61edd7d82d1721e934e7251b8c38
Wanderlei Souza <wandi.darko@gmail.com>
2020-01-17 00:29:00 -0200

    improve docs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,7 +21,7 @@ INSERT INTO PERMISSION_ROLE (PERMISSION_ID, ROLE_ID) VALUES
      (3,3),
      (4,3); /* delete -> admin */
      
-insert into user (id, username, password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('1', 'jane-doe','{bcrypt}$2y$10$xCZF3WfYHXxw9LVuj/wWm.jp4AXm4bImdZtWcFGDfEo8s5ZEvWJ9us', 'jane@mail.com', '1', '1', '1', '1');
+insert into user (id, username, password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('1', 'jane-doe','{bcrypt}$2y$10$KJqQfEwLL3G.dcyMb0xZqeUKGv0cL0.NZN5DwVsNyRfa4Mjm/qYlS', 'jane@mail.com', '1', '1', '1', '1');
 insert into user (id, username, password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('2', 'john-doe','{bcrypt}$2y$10$3qO2b9/onOhP3UW8LE9T7euvP4gWX4fqF.Gf7Z86NOnaVXf8CV3Au', 'john@mail.com', '1', '1', '1', '1');
 insert into user (id, username, password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('3', 'judite','{bcrypt}$2y$10$MXXgXo51qxETRwhjBuLGOOlPaZgCslwwSqfRRm1zA7Pdro00/Oxpy', 'judite@mail.com', '1', '1', '1', '1');
 insert into user (id, username, password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('4', 'chuck-norris','{bcrypt}$2y$10$xKQLivLpDXRqTHsYuNJVZemfp5P5rfZ5kFbvovxigESNWFH3mFYzy', 'chuck@mail.com', '1', '1', '1', '1');

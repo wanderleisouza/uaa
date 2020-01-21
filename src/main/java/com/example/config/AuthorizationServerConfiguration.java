5e1f56974c930c04b8071132d74d52327132ba33
Wanderlei Souza <wandi.darko@gmail.com>
2020-01-21 20:57:42 -0200

    JWT implementation and remove all database  warnings ;)

--- src/main/java/com/example/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/example/config/AuthorizationServerConfiguration.java
@@ -1,8 +1,11 @@
 package com.example.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -10,9 +13,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 public class AuthorizationServerConfiguration implements AuthorizationServerConfigurer {
@@ -24,10 +27,12 @@ public class AuthorizationServerConfiguration implements AuthorizationServerConf
     @Autowired
     private AuthenticationManager authenticationManager;
 
-
+    private static final char[] KEY_STORE_PASSWORD = "password".toCharArray();
+    
     @Bean
     TokenStore jdbcTokenStore() {
-        return new JdbcTokenStore(dataSource);
+        //return new JdbcTokenStore(dataSource);
+    	return new JwtTokenStore(jwtAccessTokenConverter());
     }
 
     @Override
@@ -42,7 +47,16 @@ public class AuthorizationServerConfiguration implements AuthorizationServerConf
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(jdbcTokenStore());
+        //endpoints.tokenStore(jdbcTokenStore());
+    	endpoints.tokenStore(jdbcTokenStore()).tokenEnhancer(jwtAccessTokenConverter());
         endpoints.authenticationManager(authenticationManager);
     }
+    
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() { 
+    	JwtAccessTokenConverter jwtAccessTokenConverter = new CustomTokenEnhancer();
+    	jwtAccessTokenConverter.setKeyPair(new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), KEY_STORE_PASSWORD).getKeyPair("jwt"));
+    	return jwtAccessTokenConverter;
+    }
+    
 }
